10340d1a4879766a45d6b68146ca1f0dae3e2d11
bakaYar0 <fpa.aff.air@gmail.com>
2021-12-12 20:39:51 +0300

    developing CatalogLogic / learning to use dbContext

--- bookStore v.0.02/Logic/CatalogLogic.cs
+++ bookStore v.0.02/Logic/CatalogLogic.cs
@@ -1,39 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using bookStore_v._0._02.Models;
+using Newtonsoft.Json;
 
 namespace bookStore_v._0._02.Logic
 {
-    class CatalogLogic
+    public static class CatalogLogic
     {
-        public List<Book> FindBooksByField(string field)
+        public static void InitializeDB()
+        {
+            var booksJson = File.ReadAllText(@"D:\Projects\repos\bookStore v.0.02\ProgrammersLibrary.json");
+            Book[] bookShelf = JsonConvert.DeserializeObject<Book[]>(booksJson);
+
+            using var bookShop = new BookShopContext();
+            bookShop.Books.AddRange(bookShelf);
+            bookShop.SaveChanges();
+        }
+        public static List<Book> FindBooksByField(string field)
         {
             return new List<Book>();
         }
-        public Book FindBookByISBN(string isbn)
+        public static Book FindBookByID(string id)
         {
-            return new Book();
+            using var bookShop = new BookShopContext();
+            int intID = int.Parse(id);
+            return bookShop.Books.Single(x => x.BookID == intID);
         }
-        public bool CreateBook(Book book)
+        public static void CreateBook(string book)
         {
-            return false;
+            using var bookShop = new BookShopContext();
+            Book newBook = JsonConvert.DeserializeObject<Book>(book);
+            bookShop.Books.Add(newBook);
+            bookShop.SaveChanges();
         }
-        public bool EditBook(Book book)
-        {
-            return false;
-        }
-        public List<Book> ShowCatalog()
-        {
-            return new List<Book>();
-        }
-        public bool DeleteBook(Book book)
+        public static bool EditBook(Book book)
         {
             return false;
         }
-        public bool StockUpdate(string id, int amount)
+        public static List<Book> ShowCatalog()
+        {
+            using var bookShop = new BookShopContext();
+            return bookShop.Books.OrderBy(x => x.BookID).ToList();
+        }
+        public static void DeleteBook(Book book)
+        {
+            using var bookShop = new BookShopContext();
+            bookShop.Books.Remove(book);
+            bookShop.SaveChanges();
+        }
+        public static bool StockUpdate(string id, int amount)
         {
             return false;
         }
--- bookStore v.0.02/Program.cs
+++ bookStore v.0.02/Program.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 using System.IO;
 using System.Linq;
 using System.Data.Entity;
+using bookStore_v._0._02.Logic;
 
 namespace bookStore_v._0._02
 {
@@ -11,25 +12,10 @@ namespace bookStore_v._0._02
     {
         static void Main(string[] args)
         {
-            var booksJson = File.ReadAllText(@"D:\Projects\repos\bookStore v.0.02\jsconfig1.json");
-            Book[] bookShelf = JsonConvert.DeserializeObject<Book[]>(booksJson);
+            var bookshelf = CatalogLogic.ShowCatalog();
 
-            using (var bookShop = new BookShopContext())
-            { 
-                bookShop.Books.AddRange(bookShelf);
-                bookShop.SaveChanges();
-            }
-
- 
-            var titleSortedBookshelf = bookShelf.OrderBy(x => x.Title);
-
-            var valueSortedBookshelf = bookShelf.OrderByDescending(x => x.Value);
-
-            var authorSortedBookshelf = bookShelf.OrderBy(x => x.Author);
-
-            var authorList = bookShelf.Select(x => x.Author).OrderBy(x => x);
-
-            var publisher = bookShelf.GroupBy(x => x.Publisher).OrderBy(x => x.Key).ToList();
+            foreach (var book in bookshelf)
+                Console.WriteLine(book.Author + "\n" + book.Title + "\n" + book.Pages + "\n" + book.Value + "\n");
         }
     }
 }
