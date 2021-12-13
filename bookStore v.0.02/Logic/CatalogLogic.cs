d21ef7c7421026a0830bba4d2e0315da0d78027f
bakaYar0 <fpa.aff.air@gmail.com>
2021-12-13 20:26:15 +0300

    adding more catalog logic & testing with program.cs

--- bookStore v.0.02/Logic/CatalogLogic.cs
+++ bookStore v.0.02/Logic/CatalogLogic.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using bookStore_v._0._02.Models;
@@ -22,24 +23,63 @@ namespace bookStore_v._0._02.Logic
         }
         public static List<Book> FindBooksByField(string field)
         {
-            return new List<Book>();
+            using var bookShop = new BookShopContext();
+            try
+            {
+                return bookShop.Books.Where(x => x.Author.Contains(field) ||
+                                                x.ISBN.Contains(field) ||
+                                                x.Title.Contains(field) ||
+                                                x.Description.Contains(field)).ToList();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Books are not found \n" + e.Message);
+                return new List<Book>(); //what do we need to return ? 
+            }
         }
         public static Book FindBookByID(string id)
         {
             using var bookShop = new BookShopContext();
-            int intID = int.Parse(id);
-            return bookShop.Books.Single(x => x.BookID == intID);
+            try
+            {
+                int intID = int.Parse(id);
+                return bookShop.Books.Single(x => x.BookID == intID);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("The Book is not found \n" + e.Message);
+                return new Book(); //what do we need to return ? 
+            }
         }
         public static void CreateBook(string book)
         {
             using var bookShop = new BookShopContext();
-            Book newBook = JsonConvert.DeserializeObject<Book>(book);
-            bookShop.Books.Add(newBook);
-            bookShop.SaveChanges();
+            try
+            {
+                Book newBook = JsonConvert.DeserializeObject<Book>(book);
+                bookShop.Books.Add(newBook);
+                bookShop.SaveChanges();
+                Console.WriteLine("Addition Successful");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("The Book was not created" + e.Message);
+            }
+            
         }
-        public static bool EditBook(Book book)
+        public static void EditBook(Book book, string field, string edit)
         {
-            return false;
+            using var bookShop = new BookShopContext();
+            try
+            {
+                var bookToEdit = bookShop.Books.Single(x => x.BookID == book.BookID);
+                //TODO
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }      
         }
         public static List<Book> ShowCatalog()
         {
@@ -49,12 +89,33 @@ namespace bookStore_v._0._02.Logic
         public static void DeleteBook(Book book)
         {
             using var bookShop = new BookShopContext();
-            bookShop.Books.Remove(book);
-            bookShop.SaveChanges();
+            try
+            {
+                var bookToDelete = bookShop.Books.Single(x => x.BookID == book.BookID);
+                bookShop.Books.Remove(bookToDelete);
+                bookShop.SaveChanges();
+                Console.WriteLine("Deletion successful");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
-        public static bool StockUpdate(string id, int amount)
+        public static void StockUpdate(string id, int amount)
         {
-            return false;
+            int intID = int.Parse(id);
+            using var bookShop = new BookShopContext();
+            try
+            {
+                var bookToUpdate = bookShop.Books.Single(x => x.BookID == intID);
+                bookToUpdate.AmountInStock = amount;
+                bookShop.SaveChanges();
+                Console.WriteLine("Update successful");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
