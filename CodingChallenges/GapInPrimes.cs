1c12fe0f1f0e3222d5037865e0f8e70aaf9fa3ac
Pavel Fedorov <87358389+bakaYaR0@users.noreply.github.com>
2021-12-27 11:25:11 +0300

    Add files via upload

---
+++ CodingChallenges/GapInPrimes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class GapInPrimes
+{
+    public static long[] Gap(int g, long m, long n)
+    {
+        // your code
+        for (long i = m; i <= n - g; i++)
+        {
+            long first = i;
+            long second = i + g;
+            if (GapInPrimes.IsPrime(first) & GapInPrimes.IsPrime(second))
+            {
+                if (!ContainsPrimes(first, second))
+                    return new long[] { first, second };
+            }
+        }
+        return null;
+    }
+
+    public static bool ContainsPrimes(long first, long second)
+    {
+        for (long i = first + 1; i < second; i++)
+        {
+            if (GapInPrimes.IsPrime(i))
+                return true;
+        }
+        return false;
+    }
+
+    public static bool IsPrime(long number)
+    {
+        if (number % 2 == 0) return false;
+
+        var boundary = (long)Math.Floor(Math.Sqrt(number));
+
+        for (long i = 3; i <= boundary; i += 2)
+            if (number % i == 0)
+                return false;
+
+        return true;
+    }
+}
