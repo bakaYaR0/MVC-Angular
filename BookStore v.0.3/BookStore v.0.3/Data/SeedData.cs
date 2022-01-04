19b87fa9eb6aef0ce50de4f06c25bec87a8b097d
bakaYar0 <fpa.aff.air@gmail.com>
2022-01-04 14:36:35 +0300

    Creating a separate class for Data Seed

---
+++ BookStore v.0.3/BookStore v.0.3/Data/SeedData.cs
@@ -0,0 +1,32 @@
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
+
+
+namespace BookStore.Data
+{
+    public static class SeedData
+    {
+        public static void InitializeDB(IServiceProvider serviceProvider)
+        {
+            using (var context = new BookShopContext(
+                serviceProvider.GetRequiredService<DbContextOptions<BookShopContext>>()))
+            {
+                if (context.Books.Any())
+                {
+                    return;
+                }
+
+                var booksJson = File.ReadAllText(@"D:\Git\MVC-Angular\BookStore v.0.3\BookStore v.0.3\ProgrammersLibrary.json");
+                Book[] bookShelf = JsonConvert.DeserializeObject<Book[]>(booksJson);
+
+                context.Books.AddRange(bookShelf);
+                context.SaveChanges();
+            }
+        }
+    }
+}
