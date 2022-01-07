78c3b0ab0f6275f4ccfff23a56b50a3f09eeb4e8
bakaYar0 <fpa.aff.air@gmail.com>
2022-01-07 19:07:26 +0300

    Adding simple order logic

--- BookStore v.0.3/BookStore v.0.3/Logic/OrderLogic.cs
+++ BookStore v.0.3/BookStore v.0.3/Logic/OrderLogic.cs
@@ -1,4 +1,6 @@
 ﻿using BookStore.Data;
+using BookStore.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,24 +17,73 @@ namespace BookStore.Logic
             _bookShop = context;
         }
 
-        public void MakeOrder()
+        public void MakeOrder(string order)
         {
+            try
+            {
+                Order newOrder = JsonConvert.DeserializeObject<Order>(order);
+                _bookShop.Orders.Add(newOrder);
+                _bookShop.SaveChanges();
+                Console.WriteLine("Addition Successful");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The Book was not created" + "\n" + e.Message + "\n" + e.StackTrace);
+            }
 
         }
 
-        public void ShowOrderInfo()
+        public async void EditOrder(Order order)
         {
+            try
+            {
+                var orderToEdit = _bookShop.Orders.Single(x => x.OrderID == order.OrderID);
+                
+                
+                if (order == null || orderToEdit == null)
+                    throw new Exception("Source or/and Destination Objects are null");
+                if (orderToEdit.Status != "Complete")
+                {
+                    orderToEdit.Address = order.Address;
+                    orderToEdit.DeliveryType = order.DeliveryType;
+                    orderToEdit.PhoneNumber = order.PhoneNumber;
+                    orderToEdit.Status = order.Status;
+                    if (orderToEdit.Status != "Processed")
+                    {
+                        orderToEdit.OrderItems = order.OrderItems;
+                        orderToEdit.OrderValue = order.OrderValue;
+                    }
+                }
 
+                await _bookShop.SaveChangesAsync();
+                Console.WriteLine("Edit Successful");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + "\n" + e.StackTrace);
+            }
         }
 
-        public void EditOrder()
+        public async void ChangeOrderStatus(Order order)
         {
+            try
+            {
+                var orderToEdit = _bookShop.Orders.Single(x => x.OrderID == order.OrderID);
 
-        }
+                if (order == null || orderToEdit == null)
+                    throw new Exception("Source or/and Destination Objects are null");
+                if (orderToEdit.Status != "Complete")
+                {
+                    orderToEdit.Status = order.Status;
+                }
 
-        public void CloseOrder()
-        {
-
+                await _bookShop.SaveChangesAsync();
+                Console.WriteLine("Update Successful");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + "\n" + e.StackTrace);
+            }
         }
     }
 }
