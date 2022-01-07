22d5e2848baf52abf655eaf42ff71ac798969501
bakaYar0 <fpa.aff.air@gmail.com>
2022-01-07 19:07:40 +0300

    Adding simple profile logic

--- BookStore v.0.3/BookStore v.0.3/Logic/ProfileLogic.cs
+++ BookStore v.0.3/BookStore v.0.3/Logic/ProfileLogic.cs
@@ -1,5 +1,6 @@
 ﻿using BookStore.Data;
 using BookStore.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,24 +17,59 @@ namespace BookStore.Logic
             _bookShop = context;
         }
 
-        public Profile CreateProfile()
+        public void CreateProfile(string profile)
         {
-            return new Profile();
+            try
+            {
+                Profile newProfile = JsonConvert.DeserializeObject<Profile>(profile);
+                _bookShop.Profiles.Add(newProfile);
+                _bookShop.SaveChanges();
+                Console.WriteLine("Addition Successful");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The Profile was not created" + "\n" + e.Message + "\n" + e.StackTrace);
+            }
         }
 
-        public void ShowProfileInfo()
+        public async void EditProfile(Profile profile)
         {
+            try
+            {
+                var profileToEdit = _bookShop.Profiles.Single(x => x.ProfileID == profile.ProfileID);
+                if (profile == null || profileToEdit == null)
+                    throw new Exception("Source or/and Destination Objects are null");
+
+                profileToEdit.Login = profile.Login;
+                profileToEdit.Email = profile.Email;
+                profileToEdit.Birthdate = profile.Birthdate;
+                profileToEdit.Password = profile.Password;
+                profileToEdit.PhoneNumber = profile.PhoneNumber;
+                profileToEdit.UserRole = profile.UserRole;
+
+                await _bookShop.SaveChangesAsync();
+                Console.WriteLine("Edit Successful");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + "\n" + e.StackTrace);
+            }
 
         }
 
-        public void EditProfile()
+        public async void DeleteProfile(Profile profile)
         {
-
-        }
-
-        public void DeleteProfile()
-        {
-
+            try
+            {
+                var profileToDelete = _bookShop.Profiles.Single(x => x.ProfileID == profile.ProfileID);
+                _bookShop.Profiles.Remove(profileToDelete);
+                await _bookShop.SaveChangesAsync();
+                Console.WriteLine("Deletion successful");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + "\n" + e.StackTrace);
+            }
         }
     }
 }
