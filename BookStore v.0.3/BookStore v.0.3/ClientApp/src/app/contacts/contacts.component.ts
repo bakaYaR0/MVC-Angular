588df00e491e84eade012d7bb3fc37c2c52673f9
bakaYar0 <fpa.aff.air@gmail.com>
2022-01-29 19:32:18 +0300

    Adding Contacts

---
+++ BookStore v.0.3/BookStore v.0.3/ClientApp/src/app/contacts/contacts.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts',
+  templateUrl: './contacts.component.html',
+  styles: []
+})
+export class ContactsComponent {
+  title = 'Best Book Store ever!';
+}
