70960de3843b161757d638cb20e631093b6a7e56
bakaYar0 <fpa.aff.air@gmail.com>
2022-02-22 11:34:38 +0300

    Updating app.component

--- BookStore v.0.3/BookStore v.0.3/ClientApp/src/app/app.component.ts
+++ BookStore v.0.3/BookStore v.0.3/ClientApp/src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styles: []
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
+
     title = 'Best Book Shop Ever';
-    constructor(private router: Router) { }
-    ngOnInit() { this.redirect() }
-    redirect() {
-        this.router.navigate(['home']);
+
+    ngOnInit() {
     }
+
 }
