b70b50862ee8eb0def28be1cc27ee8834a72c606
bakaYar0 <fpa.aff.air@gmail.com>
2022-02-24 01:01:10 +0300

    making fixing bug with product.details

--- BookStore v.0.3/BookStore v.0.3/ClientApp/src/app/components/product-details/product-details.component.ts
+++ BookStore v.0.3/BookStore v.0.3/ClientApp/src/app/components/product-details/product-details.component.ts
@@ -1,14 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Book } from 'src/app/models/book';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html'
+    selector: 'app-product-details',
+    templateUrl: './product-details.component.html'
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+    id!: any;
+    book!: Book;
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private dataService: DataService,
+        private route: ActivatedRoute,
+        private router: Router
+    ) { }
 
+    ngOnInit(): void {
+
+        this.id = this.route.snapshot.params['bookID'];
+        this.dataService.getProduct(this.id)
+            .subscribe({
+                next: (data) => {
+                this.book = data;
+                console.log(data);
+            },
+                error: (e) => console.error(e)
+            });
+    }
+
+    deleteBook(): void {
+        this.dataService.deleteProduct(this.id)
+            .subscribe({
+                next: (res) => {
+                    console.log(res);
+                    this.router.navigate(['/catalog']);
+                },
+                error: (e) => console.error(e)
+            });           
+    }
 }
