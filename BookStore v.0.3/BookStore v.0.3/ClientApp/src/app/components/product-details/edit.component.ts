f3aaf6a1e0991b51834a61d50ed54240b9770c41
bakaYar0 <fpa.aff.air@gmail.com>
2022-02-24 01:01:36 +0300

    adding edit details component

---
+++ BookStore v.0.3/BookStore v.0.3/ClientApp/src/app/components/product-details/edit.component.ts
@@ -0,0 +1,40 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Book } from 'src/app/models/book';
+import { DataService } from 'src/app/services/data.service';
+
+@Component({
+    selector: 'app-edit',
+    templateUrl: './edit.component.html'
+})
+
+export class EditComponent implements OnInit {
+
+    id!: any;
+    book!: Book;
+    form!: FormGroup;
+
+    constructor(
+        private dataService: DataService,
+        private route: ActivatedRoute,
+        private router: Router
+    ) { }
+
+    ngOnInit(): void {
+        this.id = this.route.snapshot.params['bookID'];
+        this.dataService.getProduct(this.id)
+            .subscribe((data: Book) => {
+            this.book = data;
+        });
+    }
+
+    saveBook(): void {
+        console.log(this.book);
+        this.dataService.updateProduct(this.id, this.book)
+            .subscribe((res: any) => {
+                console.log();
+            this.router.navigateByUrl('catalog/' + this.book.bookID + '/details');
+            })
+    }
+}
